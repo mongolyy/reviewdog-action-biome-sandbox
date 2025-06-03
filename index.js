c05e96e4b5bdc01b4af8986cd5808a2c71a45369
mongolyy <mongolyy@gmail.com>
2025-06-04 07:39:52 +0900

    roll back index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const user_name = "john";
-const api_key = "secret";
-
-console.info("This should trigger an error");
-console.log("This should be allowed");
-
-const unusedVariable = "not used anywhere";
-
-function processUser() {
-    return user_name + api_key;
-}
-
-export { processUser };
+[a a, ++b, ] = test;
